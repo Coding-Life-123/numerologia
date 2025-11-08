92bca5fbae1006b91002fae571328dfe8fc6b411
Coding-Life-123 <dfordonezpina@gmail.com>
2025-11-08 15:44:27 -0500

    new files

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
 import express from 'express'
 import 'dotenv/config'
 
+const app = express();
+
+app.use(express.json());
+
+app.use('/api/usuarios', userRoutes);
+app.use('/api/producto', numRoutes);
